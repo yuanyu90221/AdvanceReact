2cf890111bf6d072928589f8bb90a0261b117f8a
yuanyu90221 <yuanyu90221@gmail.com>
2018-12-27 00:22:01 +0800

    add style

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,17 @@
+// import React, { Component } from 'react';
+
+// class Home extends Component {
+//   render() {
+//     return <p>Hey!</p>
+//   }
+// }
+import Link from 'next/link';
+const Home = props => (
+  <div>
+    <p>Hey1213</p>
+    
+    
+  </div>
+)
+
+export default Home;
